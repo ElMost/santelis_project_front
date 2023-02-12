864b2e5ba24d855cb6216d9605d35289fa607a5d
Mouss <elmost777@gmail.com>
2023-02-12 23:06:27 +0100

    mise en place de la navbar et des navlink

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/navbar/NavBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Container>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+      </Routes>
+      </BrowserRouter>
+      
+
       <h1>Bienvenue sur Santelis</h1>
-    </Container>
+    </div>
   );
 }
 
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import logo from '../../assets/logo.png';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+
+const NavBar = () => {
+    const[navbar, setNavbar] = useState(false);
+    const[isAuthenticated, setIsAuthenticated] = useState(false);
+    
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light bg-light shadow"
+    
+    >
+      <div className=" container-fluid" >
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="logo" width={70} height={70} />
+        </a>
+        
+        <button
+            className="navbar-toggler-icon"
+            type="button"
+          onClick={() => setNavbar(!navbar)}
+        >
+          {/* <span className="navbar-toggler-icon"></span> */}
+        </button>
+        <div className={`collapse navbar-collapse ${navbar ? 'show' : ''}`}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to={'quisommesnous'} className="nav-link">
+                Qui sommes-nous
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={'nosservices'} className="nav-link">
+                Nos services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={'notreengagement'} className="nav-link">
+                Notre engagement
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={'postuler'} className="nav-link">
+                Postuler
+              </NavLink>
+            </li>
+            {isAuthenticated ? (
+              <li className="nav-item">
+                <NavLink to={'/login'} className="nav-link">
+                  Inscription/Connexion
+                </NavLink>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <NavLink to={'/dashboard'} className="nav-link">
+                  Tableau de bord
+                </NavLink>
+              </li>
+            )}
+          </ul>
+          <div className="d-flex">
+            <div className="nb-1 me-2">
+              <NavLink to="demande" className="btn btn-danger">
+                Devis gratuit
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
